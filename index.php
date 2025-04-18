9e656e91717e5a2b231f27a5b851ab582db5cd64
Eineleine <dsa210487@mail.ru>
2025-04-19 02:43:37 +0300

    Task exercise-02 of 003-operators is ready

--- index.php
+++ index.php
@@ -9,13 +9,18 @@ $family = trim(fgets(STDIN));
 echo "Введите отчество: \n";
 $surname = trim(fgets(STDIN));
 
+$name = mb_convert_case($name, MB_CASE_TITLE, "UTF-8");
+$family = mb_convert_case($family, MB_CASE_TITLE, "UTF-8");
+$surname = mb_convert_case($surname, MB_CASE_TITLE, "UTF-8");
+
 $fullname = ucwords($family . ' ' . $name . ' ' . $surname);
-$arrName = str_split($name, 1);
-$arrSurname = str_split($surname, 1);
-$arrFamily = str_split($family, 1);
+$arrName = mb_str_split($name, 1, "UTF-8");
+$arrSurname = mb_str_split($surname, 1, "UTF-8");
+$arrFamily = mb_str_split($family, 1, "UTF-8");
 $surnameAndInitials = ucwords($family . ' ' . $arrName[0] . '.' . ucfirst($arrSurname[0]) . '.');
 $fio = strtoupper($arrFamily[0] . $arrName[0] . $arrSurname[0]);
 
 fwrite(STDOUT, "Полное имя: '$fullname'" . PHP_EOL);
 fwrite(STDOUT, "Фамилия и инициалы: '$surnameAndInitials'" . PHP_EOL);
 fwrite(STDOUT, "Аббревиатура: '$fio'" . PHP_EOL);
+
